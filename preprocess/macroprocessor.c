d1a2ea72e6b4ba057b1380eabb3d712fc1a04c6b
Yehonatan Avrahimi <yontank@asus-zen.lan>
2025-05-20 18:40:28 +0300

    I Forgot to format Files
    Pleaes, PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE
    PLEASEPLEASEPLESAEPLSESAEPLASE

--- preprocess/macroprocessor.c
+++ preprocess/macroprocessor.c
@@ -6,7 +6,6 @@ int main(int argc, char *argv[]) {
   FILE *in = stdin, *out;
   char line[MAX_LINE];
 
-
   if (argc < 2) {
     fprintf(stderr, "Usage %s input_file [output_file]\n", argv[0]);
     exit(EXIT_FAILURE);
@@ -21,9 +20,9 @@ int main(int argc, char *argv[]) {
     exit(EXIT_FAILURE);
   }
 
-  while (!feof(in)){
-  fgets(line, MAX_LINE, in);
-  printf("line: %s", line);
+  while (!feof(in)) {
+    fgets(line, MAX_LINE, in);
+    printf("line: %s", line);
   }
 
   return EXIT_SUCCESS;
