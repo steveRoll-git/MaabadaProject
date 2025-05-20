67e07aeda773126f1991eaae0ad2c65c55b6ac2a
Yehonatan Avrahimi <yontank@asus-zen.lan>
2025-05-20 18:36:14 +0300

    Init - Macro Processor

---
+++ preprocess/macroprocessor.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_LINE 81
+
+int main(int argc, char *argv[]) {
+  FILE *in = stdin, *out;
+  char line[MAX_LINE];
+
+
+  if (argc < 2) {
+    fprintf(stderr, "Usage %s input_file [output_file]\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  /* Open Read File, check that it works wonderfully.*/
+  in = fopen(argv[1], "r");
+  out = fopen("output.asm", "w");
+
+  if (in == NULL) {
+    fprintf(stderr, "Couldn't open Input File\n");
+    exit(EXIT_FAILURE);
+  }
+
+  while (!feof(in)){
+  fgets(line, MAX_LINE, in);
+  printf("line: %s", line);
+  }
+
+  return EXIT_SUCCESS;
+}
