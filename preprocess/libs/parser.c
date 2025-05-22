b9810845feafaf84a8a1e7e8bc1b7d1391d64480
Yehonatan Avrahimi <yontank@asus-zen.lan>
2025-05-22 11:18:52 +0300

    INIT: line parser file

---
+++ preprocess/libs/parser.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+
+#define ASSERT(condition, message, is_critical)                                \
+  if (!(condition)) {                                                          \
+    printf("%s", message);                                                     \
+    if (is_critical)                                                           \
+      exit(1);                                                                 \
+    return 1;                                                                  \
+  }
+
+enum LINE_TYPE { INFORMATIVE, COMMAND, WHITESPACE, COMMENT, UNKNOWN };
+
+enum LINE_TYPE line_type(char *line) {
+  int len = strlen(line);
+  if (*line == ';') {
+    return COMMENT;
+  }
+
+  /*TODO: This function checks what types of command we're in, label has to be
+     checked else where (since it's optional.) */
+  // if (*(line + (len - 1)) == ':') {
+  //   /* Checks if the last value is a colon, meaning it's a label.*/
+  //   return
+  // }
+
+  else if (*line == '.') {
+    /* This is  For  Informative sentence, for whenever we receieve */
+    return INFORMATIVE;
+  }
+
+  /*TODO: Create a a checker for Command sentences. */
+  return UNKNOWN;
+}
+
+int is_label(char *token) {
+  int len = strlen(token);
+
+  return *(token + (len - 1)) == ':';
+}
+
+/*FIXME: add global header file for MAX_LINE length, so i won't have to set it
+ * manually here.*/
+void parse_line(char line[81]) {
+  /* Does Line Contain Label? (Something like variable name)*/
+  char *token = strtok(line, " \t");
+  char *label = NULL;
+
+  if (is_label(token)) {
+    memcpy(label, token, strlen(token) - 1);
+    printf("Label is: %s", label);
+  }
+}
---
+++ preprocess/libs/parser.h
@@ -0,0 +1,17 @@
+
+
+/** Recieves a token from the user, which then gets analyzed to check which type
+ * of line it is. Right now, there are 4 type of line 1) WhiteSpace line being
+ * only filled with empty spaces 2) Comments, starting with semi-colon (;) 3)
+ * Informative, Creating Variables, letting the user know about stuff. 4)
+ * Commands, the raw ASM commands written.
+ *
+ */
+void line_type(char *line);
+
+/* FIXME: add global header for MAX_LINE length.*/
+
+/** Receievs a Line of MAX_LENGTH size, and parses the information to check if
+ * it's correct.*/
+void parse_line(char line[81]);
+
