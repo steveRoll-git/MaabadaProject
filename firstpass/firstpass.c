3614f1c0d81e1b8ebcd6d72a686760246d263017
Yehonatan Avrahimi <yontank@asus-zen.lan>
2025-06-23 22:53:08 +0300

    init firtpass

---
+++ firstpass/firstpass.c
@@ -0,0 +1,8 @@
+#include "../common/linked_list.h"
+int main(int argc, char **argv) {
+  /*Instruction counter, and Data counter initilization.*/
+  int i_counter = 100, d_counter = 0;
+  linked_list_t data_table = list_init();
+  linked_list_t instruction_table = list_init();
+
+}
