bcaa1cea89c2b78665b795a64c28890f61813f53
Yehonatan Avrahimi <48332126+yontank@users.noreply.github.com>
2025-06-28 20:42:46 +0300

    Changing Header File names after changing file structure

--- preprocess/macroprocessor.c
+++ preprocess/macroprocessor.c
@@ -1,5 +1,5 @@
-#include "../common/linked_list.h"
-#include "libs/parser.h"
+#include "../datatypes/linked_list.h"
+#include "parser.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
