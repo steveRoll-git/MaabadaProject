fc946cd66fac6f7257fabba3a63272522315e5eb
Yehonatan Avrahimi <48332126+yontank@users.noreply.github.com>
2025-06-29 17:10:53 +0300

    File Restrucutre: (Sorry Ron!) Adding More tests.

--- tests/main_tester.c
+++ tests/main_tester.c
@@ -1,7 +1,8 @@
+
 #include "lib/unity.h"
 #include "lib/unity_internals.h"
-#include "../external/data.h"
-#include "../external/utils.h"
+#include "../common/data.h"
+#include "../common/utils.h"
 #include "../datatypes/linked_list.h"
 #include "../firstpass/parser.h"
 
@@ -13,79 +14,64 @@ void tearDown(void) {
     // clean stuff up here
 }
 
-void test_function_should_doBlahAndBlah(void) {
-    //test stuff
+void test_one_arg_no_args(void){
+    char *s = "                             ";
+    args_t arg = ONE_ARG;
+    int result = parse_instruction_args(&s, arg);
 
+    TEST_ASSERT_EQUAL_INT_MESSAGE(0, result, "should return false, expected arguement got EOF");
+}
+
+void test_one_arg_number_decimal(void){
+  char *s = "#-5.12";
+  args_t arg = ONE_ARG;
+  int result = parse_instruction_args(&s, arg);
+
+ TEST_ASSERT_EQUAL_INT_MESSAGE(0, result, "Assembler only accepts whole numbers");
+}
+
+void test_one_arg_number_whole(void){
+  char *s = "#-105";
+  args_t arg = ONE_ARG;
+
+  int result = parse_instruction_args(&s, arg);
+  TEST_ASSERT_EQUAL_INT_MESSAGE(1, result, "Assembler only accepts numbers");
+}
+
+void test_one_arg_number_whole_2(void){
+    char *s = "               #105                    ";
+    args_t arg = ONE_ARG;
+
+    int result = parse_instruction_args(&s, arg);
+    TEST_ASSERT_EQUAL_INT_MESSAGE(1, result, "Assembler only accepts numbers");
+}
+
+void test_one_arg_number_whole_3(void){
+    char *s = "               #           -55                    ";
+    args_t arg = ONE_ARG;
+
+    int result = parse_instruction_args(&s, arg);
+    TEST_ASSERT_EQUAL_INT_MESSAGE(0, result, "Shouldn't parse spaces between one arg.");
 }
 
 
-void test_parse_ddata_work_example1(void) {
-    //more test stuff
-    int i = 0;
-    char *token = "5  , 1 ,    -51,    29   ,18   , 2";
-    linked_list_t macro_table = list_init();
-    int val = parse_ddata(&token, &macro_table);
-
-    int* items = list_items(&macro_table);
-    int expected[] = {5, 1, -51, 29, 18 ,2};
-
-   	for(i = 0; i < 6;i++) {
-          printf("%d\t", items[i]);
-   	}
-        printf("\n");
-
-    TEST_ASSERT_EQUAL_INT_ARRAY_MESSAGE(expected, items, (6), "");
-
-    TEST_ASSERT_EQUAL_INT(1, val);
-}
-
-void test_parse_ddata_work_example2(void) {
-    int i = 0;
-    char *token = "1 , 2, 3,, 4";
-    linked_list_t macro_table = list_init();
-    int val = parse_ddata(&token, &macro_table);
-
-    int* items = list_items(&macro_table);
-    int expected[] = {1,2,3,4};
-    TEST_ASSERT_EQUAL_INT(0, val);
-}
+/*R1 => R8*/
 
 
-void test_parse_ddata_work_example3(void) {
-    int i = 0;
-    char *token = "1 , 2, 3,- 4";
-    linked_list_t macro_table = list_init();
-    int val = parse_ddata(&token, &macro_table);
 
-    int* items = list_items(&macro_table);
-    int expected[] = {1,2,3,4};
-    TEST_ASSERT_EQUAL_INT(0, val);
-}
-void test_parse_ddata_last_comma(void) {
-    int i = 0;
-    char *token = "1 , 2, 3 , ";
-    linked_list_t macro_table = list_init();
-    int val = parse_ddata(&token, &macro_table);
 
-    int* items = list_items(&macro_table);
-    TEST_ASSERT_EQUAL_INT(0, val);
-}
 
-void test_parse_ddata_empty_string(void){
-char *token = "";
-linked_list_t macro_table = list_init();
-int val = parse_ddata(&token, &macro_table);
-TEST_ASSERT_EQUAL_INT(0, val);
-}
+
 
 
 // not needed when using generate_test_runner.rb
 int main(void) {
     UNITY_BEGIN();
-    RUN_TEST(test_parse_ddata_work_example1);
-    RUN_TEST(test_parse_ddata_work_example2);
-    RUN_TEST(test_parse_ddata_work_example3);
-    RUN_TEST(test_parse_ddata_empty_string);
-    RUN_TEST(test_parse_ddata_last_comma);
+    RUN_TEST(test_one_arg_no_args);
+    RUN_TEST(test_one_arg_number_decimal);
+    RUN_TEST(test_one_arg_number_whole);
+    RUN_TEST(test_one_arg_number_whole_2);
+    RUN_TEST(test_one_arg_number_whole_3);
+
     return UNITY_END();
 }
