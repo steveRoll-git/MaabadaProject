39b2fcae4d26430a7bb591f87ee5b550381ff3c2
Ron Tseytlin <ronts390@gmail.com>
2025-06-30 17:30:35 +0300

    fixed data test after rebase

---
+++ tests/firstpass/parser/data/main_tester.c
@@ -0,0 +1,78 @@
+#include "../../../../common/utils.h"
+#include "../../../../datatypes/linked_list.h"
+#include "../../../../firstpass/parser.h"
+#include "../../../lib/unity.h"
+#include "../../../lib/unity_internals.h"
+
+void setUp(void) {
+  // set stuff up here
+}
+
+void tearDown(void) {
+  // clean stuff up here
+}
+
+void test_parse_ddata_work_example1(void) {
+  // more test stuff
+  int i = 0;
+  char *token = "5  , 1 ,    -51,    29   ,18   , 2";
+  assembler_t assembler = assembler_create();
+  int val = parse_data(&token, &assembler);
+
+  int *items = assembler.data_array.ptr;
+  int expected[] = {5, 1, -51, 29, 18, 2};
+
+  for (i = 0; i < 6; i++) {
+    printf("%d\t", items[i]);
+  }
+  printf("\n");
+
+  TEST_ASSERT_EQUAL_INT_ARRAY_MESSAGE(expected, items, 6, "");
+
+  TEST_ASSERT_EQUAL_INT(1, val);
+}
+
+void test_parse_ddata_work_example2(void) {
+  int i = 0;
+  char *token = "1 , 2, 3,, 4";
+  assembler_t assembler = assembler_create();
+  int val = parse_data(&token, &assembler);
+
+  TEST_ASSERT_EQUAL_INT(0, val);
+}
+
+
+void test_parse_ddata_work_example3(void) {
+  int i = 0;
+  char *token = "1 , 2, 3,- 4";
+  assembler_t assembler = assembler_create();
+  int val = parse_data(&token, &assembler);
+
+  TEST_ASSERT_EQUAL_INT(0, val);
+}
+void test_parse_ddata_last_comma(void) {
+  char *token = "1 , 2, 3 , ";
+  assembler_t assembler = assembler_create();
+  int val = parse_data(&token, &assembler);
+
+  TEST_ASSERT_EQUAL_INT(0, val);
+}
+
+void test_parse_ddata_empty_string(void) {
+  char *token = "";
+  assembler_t assembler = assembler_create();
+  int val = parse_data(&token, &assembler);
+  TEST_ASSERT_EQUAL_INT(0, val);
+}
+
+
+// not needed when using generate_test_runner.rb
+int main(void) {
+  UNITY_BEGIN();
+  RUN_TEST(test_parse_ddata_work_example1);
+  RUN_TEST(test_parse_ddata_work_example2);
+  RUN_TEST(test_parse_ddata_work_example3);
+  RUN_TEST(test_parse_ddata_empty_string);
+  RUN_TEST(test_parse_ddata_last_comma);
+  return UNITY_END();
+}
