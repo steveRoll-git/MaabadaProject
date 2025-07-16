7baeeaae405504571f4cf004325e02c96b2b083c
Ron Tseytlin <ronts390@gmail.com>
2025-07-16 15:38:14 +0300

    changed errors to be `extern` variables instead of macros

--- include/errors.h
+++ include/errors.h
@@ -3,76 +3,83 @@
 
 /* Any function that can succeed or fail returns this type. If the function succeeds, it returns 0, otherwise it returns
  * a pointer to the string that represents the error. */
-typedef char *result_t;
+typedef const char *result_t;
 
 /* Functions that return a `result_t` should return this if they ran successfully. */
 #define SUCCESS 0
 
-/* Instructions */
+/* Generic assembler errors */
 
-#define ERR_UNKNOWN_INSTRUCTION "Unknown instruction."
-#define ERR_UNKNOWN_DIRECTIVE "Unknown directive."
-#define ERR_NOT_ENOUGH_ARGS "Instruction did not receive enough arguments."
-#define ERR_TOO_MANY_ARGS "Instruction received too many arguments."
-#define ERR_SRC_IMMEDIATE "This instruction doesn't support immediate source operands."
-#define ERR_SRC_REGISTER "This instruction doesn't support register source operands."
-#define ERR_DST_IMMEDIATE "This instruction doesn't support immediate destination operands."
+extern result_t ERR_PREPROCESS_FAILED;
+extern result_t ERR_CODEGEN_FAILED;
+extern result_t ERR_INPUT_FILE_FAIL;
+extern result_t ERR_OUTPUT_FILE_FAIL;
+extern result_t ERR_OUT_OF_MEMORY;
 
-/* Arguments */
+/* Preprocessor errors */
 
-#define ERR_NO_ARG "Expected an argument."
-#define ERR_EXTRANEOUS_TEXT_INSTRUCTION "Extraneous text after instruction."
-#define ERR_EXTRANEOUS_TEXT_DIRECTIVE "Extraneous text after directive."
-#define ERR_INVALID_ARGUMENT "Invalid argument syntax."
-#define ERR_DIRECTIVE_EXPECTED_LABEL "This directive expects a label as a parameter."
+extern result_t ERR_INVALID_MACRO_NAME;
+extern result_t ERR_EXTRANEOUS_TEXT_MACRO;
+extern result_t ERR_EXTRANEOUS_TEXT_MCROEND;
+extern result_t ERR_LINE_TOO_LONG;
+extern result_t ERR_FSEEK_FAILED;
 
-/* Numbers */
+/* Instruction syntax errors */
 
-#define ERR_NUMBER_NOT_VALID "Invalid number."
-#define ERR_NUMBER_AFTER_HASH "A hash must immediately be followed by a number."
+extern result_t ERR_UNKNOWN_INSTRUCTION;
+extern result_t ERR_UNKNOWN_DIRECTIVE;
+extern result_t ERR_NOT_ENOUGH_ARGS;
+extern result_t ERR_TOO_MANY_ARGS;
+extern result_t ERR_SRC_IMMEDIATE;
+extern result_t ERR_SRC_REGISTER;
+extern result_t ERR_DST_IMMEDIATE;
 
-/* Labels */
+/* Argument syntax errors */
 
-#define ERR_INVALID_LABEL "Invalid label name: Must not be a reserved word."
-#define ERR_LABEL_UNDERSCORES "Label name cannot contain underscores."
-#define ERR_LABEL_TOO_LONG "Label name cannot be longer than 30 characters."
-#define ERR_LABEL_ALREADY_DEFINED "This label has already been defined in this file."
-#define ERR_LABEL_NAME_IS_MACRO "The name of this label is already used as a macro."
-#define ERR_LABEL_NOT_DEFINED "This label was not defined."
-#define ERR_LABEL_ALREADY_ENTRY "This label was already defined as an entry."
-#define ERR_LABEL_MISSING_SPACE "There must be a space after the `:` of a label."
+extern result_t ERR_NO_ARG;
+extern result_t ERR_EXTRANEOUS_TEXT_INSTRUCTION;
+extern result_t ERR_EXTRANEOUS_TEXT_DIRECTIVE;
+extern result_t ERR_INVALID_ARGUMENT;
+extern result_t ERR_DIRECTIVE_EXPECTED_LABEL;
 
-/* Matrix */
+/* Number syntax errors */
 
-#define ERR_MATRIX_OVERFLOW "Matrix has more values than it can store."
-#define ERR_MATRIX_START_BRACKET_ROW "Missing '[' for row index."
-#define ERR_MATRIX_END_BRACKET_ROW "Missing ']' for row index."
-#define ERR_MATRIX_START_BRACKET_COL "Missing '[' for column index."
-#define ERR_MATRIX_END_BRACKET_COL "Missing ']' for column index."
-#define ERR_MATRIX_NEGATIVE_STORAGE "Matrix rows and colums must be positive integers."
-#define ERR_WRONG_MATRIX_ACCESS "Matrix rows and columns can only be accessed with registers."
+extern result_t ERR_NUMBER_NOT_VALID;
+extern result_t ERR_NUMBER_AFTER_HASH;
 
-/* String */
+/* Label syntax errors */
 
-#define ERR_STRING_MISSING_QUOTE_START "String argument must start with quotation marks."
-#define ERR_STRING_MISSING_QUOTE_END "String argument must end with quotation marks."
+extern result_t ERR_INVALID_LABEL;
+extern result_t ERR_LABEL_UNDERSCORES;
+extern result_t ERR_LABEL_TOO_LONG;
+extern result_t ERR_LABEL_ALREADY_DEFINED;
+extern result_t ERR_LABEL_NAME_IS_MACRO;
+extern result_t ERR_LABEL_NOT_DEFINED;
+extern result_t ERR_LABEL_ALREADY_ENTRY;
+extern result_t ERR_LABEL_MISSING_SPACE;
 
-#define ERR_INVALID_MACRO_NAME "Macro definition doesn't contain a valid name."
-#define ERR_EXTRANEOUS_TEXT_MACRO "Extraneous text after macro definition."
-#define ERR_EXTRANEOUS_TEXT_MCROEND "Extraneous text after `mcroend`."
-#define ERR_LINE_TOO_LONG "Line is longer than 80 characters."
-#define ERR_FSEEK_FAILED "fseek failed to run."
-#define ERR_PREPROCESS_FAILED "One or more errors were encountered during preprocessing."
-#define ERR_CODEGEN_FAILED "One or more errors were encountered during code generation."
-#define ERR_INPUT_FILE_FAIL "Couldn't open input file."
-#define ERR_OUTPUT_FILE_FAIL "Couldn't open output file."
-#define ERR_OUT_OF_MEMORY "Out of memory."
+/* Matrix syntax errors */
 
+extern result_t ERR_MATRIX_OVERFLOW;
+extern result_t ERR_MATRIX_START_BRACKET_ROW;
+extern result_t ERR_MATRIX_END_BRACKET_ROW;
+extern result_t ERR_MATRIX_START_BRACKET_COL;
+extern result_t ERR_MATRIX_END_BRACKET_COL;
+extern result_t ERR_MATRIX_NEGATIVE_STORAGE;
+extern result_t ERR_WRONG_MATRIX_ACCESS;
+
+/* String syntax errors */
+
+extern result_t ERR_STRING_MISSING_QUOTE_START;
+extern result_t ERR_STRING_MISSING_QUOTE_END;
+
+/* This macro receives an expression and an error message */
+/* It may only be called in functions that return a `result_t`. */
 /* Checks that the condition is true. If not, causes the running function to return the given error message as a
  * `result_t`. */
 #define ASSERT(a, s)                                                                                                   \
   if (!(a)) {                                                                                                          \
-    return (result_t) s;                                                                                               \
+    return s;                                                                                                          \
   }
 
 /* The given expression should be one of `result_t` type. If it returns an error string, causes the running function to
@@ -87,6 +94,6 @@ typedef char *result_t;
   while (0);
 
 /* Prints an error message. */
-void print_error(char *file_path, int line_number, char *message);
+void print_error(char *file_path, int line_number, const char *message);
 
 #endif
--- source/errors.c
+++ source/errors.c
@@ -2,6 +2,71 @@
 
 #include <stdio.h>
 
-void print_error(char *file_path, int line_number, char *message) {
+/* Generic assembler errors */
+
+result_t ERR_PREPROCESS_FAILED = "One or more errors were encountered during preprocessing.";
+result_t ERR_CODEGEN_FAILED = "One or more errors were encountered during code generation.";
+result_t ERR_INPUT_FILE_FAIL = "Couldn't open input file.";
+result_t ERR_OUTPUT_FILE_FAIL = "Couldn't open output file.";
+result_t ERR_OUT_OF_MEMORY = "Out of memory.";
+
+/* Preprocessor errors */
+
+result_t ERR_INVALID_MACRO_NAME = "Macro definition doesn't contain a valid name.";
+result_t ERR_EXTRANEOUS_TEXT_MACRO = "Extraneous text after macro definition.";
+result_t ERR_EXTRANEOUS_TEXT_MCROEND = "Extraneous text after `mcroend`.";
+result_t ERR_LINE_TOO_LONG = "Line is longer than 80 characters.";
+result_t ERR_FSEEK_FAILED = "fseek failed to run.";
+
+/* Instruction syntax errors */
+
+result_t ERR_UNKNOWN_INSTRUCTION = "Unknown instruction.";
+result_t ERR_UNKNOWN_DIRECTIVE = "Unknown directive.";
+result_t ERR_NOT_ENOUGH_ARGS = "Instruction did not receive enough arguments.";
+result_t ERR_TOO_MANY_ARGS = "Instruction received too many arguments.";
+result_t ERR_SRC_IMMEDIATE = "This instruction doesn't support immediate source operands.";
+result_t ERR_SRC_REGISTER = "This instruction doesn't support register source operands.";
+result_t ERR_DST_IMMEDIATE = "This instruction doesn't support immediate destination operands.";
+
+/* Argument syntax errors */
+
+result_t ERR_NO_ARG = "Expected an argument.";
+result_t ERR_EXTRANEOUS_TEXT_INSTRUCTION = "Extraneous text after instruction.";
+result_t ERR_EXTRANEOUS_TEXT_DIRECTIVE = "Extraneous text after directive.";
+result_t ERR_INVALID_ARGUMENT = "Invalid argument syntax.";
+result_t ERR_DIRECTIVE_EXPECTED_LABEL = "This directive expects a label as a parameter.";
+
+/* Number syntax errors */
+
+result_t ERR_NUMBER_NOT_VALID = "Invalid number.";
+result_t ERR_NUMBER_AFTER_HASH = "A hash must immediately be followed by a number.";
+
+/* Label syntax errors */
+
+result_t ERR_INVALID_LABEL = "Invalid label name: Must not be a reserved word.";
+result_t ERR_LABEL_UNDERSCORES = "Label name cannot contain underscores.";
+result_t ERR_LABEL_TOO_LONG = "Label name cannot be longer than 30 characters.";
+result_t ERR_LABEL_ALREADY_DEFINED = "This label has already been defined in this file.";
+result_t ERR_LABEL_NAME_IS_MACRO = "The name of this label is already used as a macro.";
+result_t ERR_LABEL_NOT_DEFINED = "This label was not defined.";
+result_t ERR_LABEL_ALREADY_ENTRY = "This label was already defined as an entry.";
+result_t ERR_LABEL_MISSING_SPACE = "There must be a space after the `:` of a label.";
+
+/* Matrix syntax errors */
+
+result_t ERR_MATRIX_OVERFLOW = "Matrix has more values than it can store.";
+result_t ERR_MATRIX_START_BRACKET_ROW = "Missing '[' for row index.";
+result_t ERR_MATRIX_END_BRACKET_ROW = "Missing ']' for row index.";
+result_t ERR_MATRIX_START_BRACKET_COL = "Missing '[' for column index.";
+result_t ERR_MATRIX_END_BRACKET_COL = "Missing ']' for column index.";
+result_t ERR_MATRIX_NEGATIVE_STORAGE = "Matrix rows and columns must be positive integers.";
+result_t ERR_WRONG_MATRIX_ACCESS = "Matrix rows and columns can only be accessed with registers.";
+
+/* String syntax errors */
+
+result_t ERR_STRING_MISSING_QUOTE_START = "String argument must start with quotation marks.";
+result_t ERR_STRING_MISSING_QUOTE_END = "String argument must end with quotation marks.";
+
+void print_error(char *file_path, int line_number, const char *message) {
   printf("Error at %s:%d: %s\n", file_path, line_number, message);
 }
