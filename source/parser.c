bf3c8b694d7f425aa6336a01cdb4a00dbc2936f7
Ron Tseytlin <ronts390@gmail.com>
2025-08-09 22:01:15 +0300

    moved `get_instruction` to `parser.c`

--- source/parser.c
+++ source/parser.c
@@ -58,9 +58,52 @@ void add_char(char **dest, char **src) {
   (*src)++;
 }
 
-/* Reads the next word (a sequence of alphanumeric characters that starts with a letter) at the string pointed to by
- * `s` */
-/* Updates `s` so that it will point to the next character after the word that was read. */
+/**
+ * Given an instruction's name, returns information about that instruction.
+ * If no instruction by that name exists, returns NULL.
+ *
+ * @param name The name of the instruction to look for.
+ * @return A pointer to the information about the instruction, or NULL if there is no such instruction.
+ */
+instruction_info_t *get_instruction(char *name) {
+  /* This array contains all needed information about every kind of instruction. */
+  static instruction_info_t instructions[] = {
+          {INSTRUCTION_MOV,  OPCODE_MOV,  TWO_ARGS, FALSE, TRUE },
+          {INSTRUCTION_CMP,  OPCODE_CMP,  TWO_ARGS, TRUE,  TRUE },
+          {INSTRUCTION_ADD,  OPCODE_ADD,  TWO_ARGS, FALSE, TRUE },
+          {INSTRUCTION_SUB,  OPCODE_SUB,  TWO_ARGS, FALSE, TRUE },
+          {INSTRUCTION_LEA,  OPCODE_LEA,  TWO_ARGS, FALSE, FALSE},
+          {INSTRUCTION_CLR,  OPCODE_CLR,  ONE_ARG,  FALSE, TRUE },
+          {INSTRUCTION_NOT,  OPCODE_NOT,  ONE_ARG,  FALSE, TRUE },
+          {INSTRUCTION_INC,  OPCODE_INC,  ONE_ARG,  FALSE, FALSE},
+          {INSTRUCTION_DEC,  OPCODE_DEC,  ONE_ARG,  FALSE, FALSE},
+          {INSTRUCTION_JMP,  OPCODE_JMP,  ONE_ARG,  FALSE, FALSE},
+          {INSTRUCTION_BNE,  OPCODE_BNE,  ONE_ARG,  FALSE, FALSE},
+          {INSTRUCTION_JSR,  OPCODE_JSR,  ONE_ARG,  FALSE, FALSE},
+          {INSTRUCTION_RED,  OPCODE_RED,  ONE_ARG,  FALSE, FALSE},
+          {INSTRUCTION_PRN,  OPCODE_PRN,  ONE_ARG,  TRUE,  FALSE},
+          {INSTRUCTION_RTS,  OPCODE_RTS,  NO_ARGS,  FALSE, FALSE},
+          {INSTRUCTION_STOP, OPCODE_STOP, NO_ARGS,  FALSE, FALSE}
+  };
+
+  /* The total number of instructions. */
+  static const int num_instructions = sizeof(instructions) / sizeof(instruction_info_t);
+
+  int i;
+
+  if (name == NULL) {
+    return NULL;
+  }
+
+  for (i = 0; i < num_instructions; i++) {
+    if (strcmp(name, instructions[i].name) == 0) {
+      return &instructions[i];
+    }
+  }
+
+  return NULL;
+}
+
 void read_word(char **s, word_t *word) {
   char *value_ptr = word->value;
 
