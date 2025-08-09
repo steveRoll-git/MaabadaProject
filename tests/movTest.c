fcdb11d799163efed1bdfb5bfe730b53efa02ebe
Ron Tseytlin <ronts390@gmail.com>
2025-08-09 22:03:09 +0300

    ga

--- tests/movTest.c
+++ tests/movTest.c
@@ -1,5 +1,7 @@
 #include "../headers/data.h"
 
+instruction_info_t *get_instruction(char *name);
+
 int main() {
   instruction_info_t *a = get_instruction("mov");
   return a->opcode == OPCODE_MOV ? 0 : 1;
